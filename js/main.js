97229a61c1a4abed73fcfca8ace297024f9403fc
Khushi Bhatnagar <bhatnagarkhushi31505@gmail.com>
2025-09-21 15:00:34 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -34,7 +34,7 @@ document.addEventListener("DOMContentLoaded", () => {
       }, step);
   }
   counter("count1", 0, 1287, 3000);
-  counter("count2", 100, 5786, 2500);
+  counter("count2", 0, 5786, 3000);
   counter("count3", 0, 1440, 3000);
   counter("count4", 0, 7110, 3000);
 });
